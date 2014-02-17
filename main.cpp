3382d359f4b5aae669b986a648b4f067d38ae615
killmous <wcaldwell.email@gmail.com>
2014-02-17 15:12:36 -0600

    Added gitignore and did iterated prisoner's dilemma where players remember last round's outcome. 	new file:   .gitignore 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 #include <cmath>
 #include "chromosome.h"
 
+bool bitAt(int offset, Chromosome chromo)
+{
+	return (chromo >> (offset - 1)) & 0b1;
+}
+
+int population = 64;
+int iterations = 500;
+int entropy = 5;
+double pm = 0.01;
+double pc = 0.4;
 double* fitness(Chromosome* pop, int popSize)
 {
+	int tournamentLength = 32;
+
 	double* ret = new double[popSize];
 	for(int i = 0; i < popSize; i++)
 	{
@@ -12,23 +24,56 @@ double* fitness(Chromosome* pop, int popSize)
 		double fitness1 = 0;
 		double fitness2 = 0;
 
-		if(!contender1 && !contender2)
+		int move1 = bitAt(1, contender1);
+		int move2 = bitAt(1, contender2);
+
+		for(int j = 0; j < tournamentLength; ++j)
 		{
-			fitness1 = 1;
-			fitness2 = 1;
-		}
-		else if(contender1 && !contender2)
-		{
-			fitness2 = 5;
-		}
-		else if(contender1 && contender2)
-		{
-			fitness1 = 3;
-			fitness2 = 3;
-		}
-		else if(!contender1 && contender2)
-		{
-			fitness1 = 5;
+			if(!move1 && !move2)
+			{
+				fitness1 += 1;
+				fitness2 += 1;
+			}
+			else if(move1 && !move2)
+			{
+				fitness2 += 5;
+			}
+			else if(move1 && move2)
+			{
+				fitness1 += 3;
+				fitness2 += 3;
+			}
+			else if(!move1 && move2)
+			{
+				fitness1 += 5;
+			}
+
+			if(j != tournamentLength - 1)
+			{
+				int temp1 = move1;
+				int temp2 = move2;
+
+				if(temp1 && temp2) //If opponent defect and i defect
+				{
+					move1 = bitAt(2, contender1);
+					move2 = bitAt(2, contender2);
+				}
+				else if(temp1 && !temp2) //If i defect and he cooperate
+				{
+					move1 = bitAt(4, contender1);
+					move2 = bitAt(3, contender2);
+				}
+				else if(!temp1 && !temp1)
+				{
+					move1 = bitAt(5, contender1);
+					move2 = bitAt(5, contender2);
+				}
+				else if(!temp1 && temp2)
+				{
+					move1 = bitAt(3, contender1);
+					move2 = bitAt(4, contender2);
+				}
+			}
 		}
 
 		ret[i - 1]	= 1.0 / (fitness1 + 1);
@@ -37,8 +82,14 @@ double* fitness(Chromosome* pop, int popSize)
 	return ret;
 }
 
+// bit 1 -> first move
+// bit 2 -> move if both defect
+// bit 3 -> move if he defect i cooperate
+// bit 4 -> move if i defect he cooperate
+// bit 5 -> nove if both cooperate
+
 int main(int argc, char **argv)
 {
-	Population dilemma(32, 1, 0.01, 0.4, fitness);
-	dilemma.run(10);
+	Population dilemma(population, entropy, pm, pc, fitness);
+	dilemma.run(iterations, 1);
 }
